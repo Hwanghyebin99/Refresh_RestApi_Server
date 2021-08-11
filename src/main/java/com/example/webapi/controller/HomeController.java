ef42fc2da324b59b33e31c9e864e764e4078b16e
황혜빈 <hebin99@naver.com>
2021-08-11 23:40:49 +0900

    연합학습 서비스 추가

--- src/main/java/com/example/webapi/controller/HomeController.java
+++ src/main/java/com/example/webapi/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.example.webapi.controller;
 
+import com.example.webapi.BasicRoundController;
+import com.example.webapi.FederatedServerImpl;
+import com.example.webapi.core.FederatedAveragingStrategy;
+import com.example.webapi.core.datasource.*;
+import com.example.webapi.core.domain.model.*;
+import com.example.webapi.core.domain.repository.ServerRepository;
 import com.example.webapi.model.User;
-import com.example.webapi.service.UserService;
+import com.example.webapi.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,12 +15,25 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.Properties;
 
 @RestController
-public class hello {
+public class HomeController {
+    private static FederatedServer federatedServer;
+
     @Autowired
     UserService userService;
+
+    @Autowired
+    FederatedService federatedService;
+
+    public HomeController() throws IOException {
+        this.federatedService.startFederatedServer();
+    }
 
     @GetMapping("/test")
     public String hello() {
